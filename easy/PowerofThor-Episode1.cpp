cb42796891a2d183ec79cebae5db29743d8c7fc6
transistor <1050181200@qq.com>
2020-04-02 16:22:55 +0800

    Update PowerofThor-Episode1.cpp

--- easy/PowerofThor-Episode1.cpp
+++ easy/PowerofThor-Episode1.cpp
@@ -2,9 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
-
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -21,12 +19,10 @@ int main()
     cin >> lightX >> lightY >> initialTX >> initialTY; cin.ignore();
     int tx = initialTX;
     int ty = initialTY;
- 
     // game loop
     while (1) {
         int remainingTurns; // The remaining amount of turns Thor can move. Do not remove this line.
         cin >> remainingTurns; cin.ignore();
-
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
         string x;
@@ -52,11 +48,18 @@ int main()
              ty++;
         }
         
-        cout << y << x <<endl;
-            
-   
-   
+        cout << y << x <<endl;        
         // A single line providing the move to be made: N NE E SE S SW W or NW
        
     }
 }
+/**
+ *屏幕坐标的位移计算
+ *屏幕坐标左上（lefttop）为（0，0） 右下（rightbottom）为最大（max，max）
+ *将a（initialTX，initialTY）移动到b（lightX，lightY）
+ *计算它们两点的差 lightX-initialTX（重点减去起始点）
+ *横坐标差x1如果大于0，则往右移动，小于则往左移动
+ *纵坐标差y1如果大于0，则向下移动，小于则向上移动
+ *绝对值abs是移动的距离
+ **/
+ 
